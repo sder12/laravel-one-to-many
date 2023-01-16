99a085d0b63075f09013622ba09282ad8b85098e
sder12 <sara.cetto@gmail.com>
2023-01-16 16:47:10 +0100

    Create columns in types table and add the foreign key to projects

--- database/migrations/2023_01_16_152907_create_types_table.php
+++ database/migrations/2023_01_16_152907_create_types_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100)->unique();
+            $table->string('slug');
             $table->timestamps();
         });
     }
